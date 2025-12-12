ef4461596edaff86416cb818659afc590ea3ba6a
Nitheesh D R <nitheeraj1@gmail.com>
2025-12-12 11:48:25 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import {navIcons, navLinks} from '#constants'
 
 const Navbar = () => {
-  return 
+  return (
     <nav>
       <div>
-        <img src="/images/logo.svg" />
+        <img src="/images/logo.svg" alt='logo' />
+        <p className='font-bold' >Nitheesh's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({id,name})=>(
+              <li key={{id}}>
+                <p >{name}</p>
+              </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({id,img})=>(
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className='icon-hover' />
+              </li>
+          ))}
+        </ul>
       </div>
     </nav>
-  
+  )
 }
 
 export default Navbar
